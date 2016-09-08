00c8057380ab4227277578b4c1f6044c7691a0ea
betaSu <313439271@qq.com>
2016-09-09 00:33:50 +0800

    v1.0.3 res.send()

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-//主页面
+/**
+ * 欢迎你使用sujs
+ * sujs是一个nodejs学习项目,欢迎也有学习node/express想法的小伙伴的加入
+ * 仓库地址：https://github.com/BetaSu/sujs
+ * 我的知乎: https://www.zhihu.com/people/jing-cang-tian
+ */
 
-var sujs=require('./lib/use');
+var sujs=require('./lib');
 
 module.exports=sujs;
