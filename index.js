8385e49246ac741322224243f26858873ebdda63
BetaSu <313439271@qq.com>
2016-09-20 13:27:00 +0800

    更新说明

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /**
  * 欢迎你使用sujs
- * sujs是一个nodejs学习项目,欢迎也有学习node/express想法的小伙伴的加入
- * 所有可用插件都在node_modules文件夹下
+ * sujs是一个nodejs学习项目,欢迎学习node/express的小伙伴加入
+ * 所有可用插件都在./main/plugins文件夹下,可在文件内查看使用方法
  * 仓库地址：https://github.com/BetaSu/sujs
+ * 玩的开心
  */
 
 var sujs=require('./main');
